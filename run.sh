44f066903456c32c6165b20fe02039f8d3826273
matt c <matt@xhec.dev>
2026-01-26 13:33:09 -0500

    i am lowering the bar while i work on the go code

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-# TODO: can this run on Windows?
-# (does it need to?)
-
-[[ ! -d venv ]] && python3 -m venv venv
-./venv/bin/pip install -r requirements.txt
-
-./venv/bin/python server.py
+./.venv/bin/python server.py
